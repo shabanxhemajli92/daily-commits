f9578763e38a2d329902f5eaf4287944f620fc54
Shaban <shabanxhemajli@live.com>
2022-09-08 14:20:10 +0200

    final version

--- arpse.py
+++ arpse.py
@@ -1,18 +1,15 @@
 import argparse
+parser=argparse.ArgumentParser()#this is where you open the parser/initstantiating the parser
+parser.add_argument("Hotels",help="There is only3 hotels(Delfin,Bellvie,Titanic)and after you type the Hotel name it is going to tell you the location")#this is where you add the arguments
+args=parser.parse_args()#this is where the parsing happens and this is where you close the functions
+if args.Hotels=="Delfin":
+    print("This is located in Antalya")
+elif args.Hotels=="Bellvie":
+        print("This is hotel is located in Bodrum ")
+elif args.Hotels=="Titanic":
+    print("This hotel is located in Cesme")
+else:
+    print("This is not a turistic destination")            
 
-# help flag provides flag help
-# store_true actions stores argument as True
-
-parser = argparse.ArgumentParser()
-   
-parser.add_argument('-o', '--output', action='store_true', 
-    help="shows output")
-parser.add_argument('-a', '--addition', 
-    help="it adds",type=int)    
-
-args = parser.parse_args()
-
-if args.output:
-    print("This is some output")
-elif args.addition:
-    print(args*2)
+#in order to add arguments to the parser, for each new argument you would have to write a new argument
+ 
