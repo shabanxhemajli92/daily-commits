d65b77f535a162892c1f1738032dce1278aa541a
Shaban <shabanxhemajli@live.com>
2022-09-09 11:02:12 +0200

    user verification

---
+++ verify_user.py
@@ -0,0 +1,10 @@
+import email
+from email.message import EmailMessage
+import re
+from verify_user import * 
+pattern="[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"#uppercase letters([A-Z]) lowercase letters[a-z] numbers[0-9]
+user_input=input()
+if(re.search(pattern,user_input)):
+  print("valid email")
+else:
+    ("invalid email")  
